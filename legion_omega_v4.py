6e68479238a55a61d310181e0633094494ce0d9c
Berserkiddy <berserkiddy@gmail.com>
2025-06-29 04:14:35 +0200

    Update legion_omega_v4.py

--- legion_omega_v4.py
+++ legion_omega_v4.py
@@ -1,3 +1,4 @@
-with open("legion_omega_v4_part1.py", "r") as f1, open("legion_omega_v4_part2.py", "r") as f2:
+with open("legion_omega_v4_part1.py", "r", encoding="utf-8") as f1, \
+     open("legion_omega_v4_part2.py", "r", encoding="utf-8") as f2:
     exec(f1.read())
     exec(f2.read())
